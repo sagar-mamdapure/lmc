e5fb5222ac8e481bd03fcaa41ed10f487ca0391e
vkumbhar94 <vkumbhar94@gmail.com>
2022-07-04 17:08:19 +0530

    fix(version): add version command to show utility version info

--- main.go
+++ main.go
@@ -4,8 +4,21 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/logicmonitor/lmc/cmd"
+import (
+	"github.com/logicmonitor/lmc/cmd"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "unknown"
+)
 
 func main() {
+	cmd.Version = version
+	cmd.Commit = commit
+	cmd.Date = date
+	cmd.BuiltBy = builtBy
 	cmd.Execute()
 }
